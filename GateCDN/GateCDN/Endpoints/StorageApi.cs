9c27e49557e920c16be63d1825360158caae22f4
KristofferRoenLie <KristofferRoenLie@gmail.com>
2023-07-14 00:54:11 +0200

    Found a way to use reflection instead of copying all the .net implementation stuff.

--- GateCDN/GateCDN/Endpoints/StorageApi.cs
+++ GateCDN/GateCDN/Endpoints/StorageApi.cs
@@ -1,8 +1,9 @@
-﻿using GateCDN.Features.Storage;
-using GateCDN.Services.Storage;
+﻿using GateCDN.Services.Storage;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Options;
+using System.Reflection;
 
 namespace GateCDN.Endpoints;
 
@@ -30,17 +31,17 @@ public static partial class StorageApi {
 		var path = Path.Combine(app.Environment.ContentRootPath, "vault", "live", "clients");
 		Directory.CreateDirectory(path);
 
-		app.UseMiddleware<GateStaticFileMiddleware>(Options.Create(new GateStaticFileOptions {
+		app.UseMiddleware<ReflectedStaticFileMiddleware>(Options.Create(new StaticFileOptions {
 			FileProvider = new PhysicalFileProvider(path),
 			RequestPath = "/apps",
 		}));
 
-		app.UseMiddleware<GateStaticFileMiddleware>(Options.Create(new GateStaticFileOptions {
+		app.UseMiddleware<StaticFileMiddleware>(Options.Create(new StaticFileOptions {
 			FileProvider = new PhysicalFileProvider(path),
 			RequestPath = "/serve",
 		}));
 
-		app.UseMiddleware<GateStaticFileMiddleware>(Options.Create(new GateStaticFileOptions {
+		app.UseMiddleware<StaticFileMiddleware>(Options.Create(new StaticFileOptions {
 			FileProvider = new PhysicalFileProvider(Path.Combine(app.Environment.ContentRootPath, "wwwroot")),
 			RequestPath = "",
 		}));
@@ -88,33 +89,118 @@ public static partial class StorageApi {
 }
 
 
-public class StaticFileServeFallback {
+public class ReflectedStaticFileMiddleware {
 
-	private readonly RequestDelegate _next;
-	private readonly Lazy<GateStaticFileMiddleware> staticFileMiddleware;
+	private readonly StaticFileOptions _options;
+	private readonly PathString _matchUrl;
+	private readonly ILogger _logger;
+	private readonly StaticFileMiddleware _middleware;
+	private readonly IContentTypeProvider _contentTypeProvider;
 
-	public StaticFileServeFallback(
+	public ReflectedStaticFileMiddleware(
 		RequestDelegate next,
 		IWebHostEnvironment hostingEnv,
-		IOptions<GateStaticFileOptions> options,
+		IOptions<StaticFileOptions> options,
 		ILoggerFactory loggerFactory
 	) {
-		_next = next;
-		staticFileMiddleware = new(new GateStaticFileMiddleware(
-			next, hostingEnv, options, loggerFactory));
+		_options = options.Value;
+		_matchUrl = _options.RequestPath;
+		_contentTypeProvider = _options.ContentTypeProvider ?? new FileExtensionContentTypeProvider();
+		_middleware = new StaticFileMiddleware(next, hostingEnv, options, loggerFactory);
+		_logger = loggerFactory.CreateLogger<StaticFileMiddleware>();
 	}
 
+	public bool ValidatePath(
+		HttpContext context,
+		PathString matchUrl,
+		out PathString subPath
+	) {
+		Type staticType = typeof(StaticFileMiddleware);
+		MethodInfo method = staticType.GetMethod("ValidatePath", BindingFlags.NonPublic | BindingFlags.Static)!;
+
+		subPath = "";
+		object[] parameters = new object[] { context, matchUrl, subPath };
+
+		bool result = (bool)method.Invoke(null, parameters)!;
+		subPath = (PathString)parameters.ElementAt(2);
+
+		return result;
+	}
+
+	public bool LookupContentType(
+		IContentTypeProvider contentTypeProvider,
+		StaticFileOptions options,
+		PathString subPath,
+		out string? contentType
+	) {
+		Type staticType = typeof(StaticFileMiddleware);
+		MethodInfo method = staticType.GetMethod("LookupContentType", BindingFlags.NonPublic | BindingFlags.Static)!;
+
+		contentType = null;
+		var parameters = new object?[] { contentTypeProvider, options, subPath, contentType };
+
+		bool result = (bool)method.Invoke(null, parameters)!;
+		contentType = (string?)parameters.ElementAt(3);
+
+		return result;
+	}
+
+	public Task TryServeStaticFile(
+		HttpContext context,
+		string? contentType,
+		PathString subPath
+	) {
+		Type staticType = typeof(StaticFileMiddleware);
+		MethodInfo method = staticType.GetMethod("TryServeStaticFile", BindingFlags.NonPublic | BindingFlags.Instance)!;
+
+		var parameters = new object?[] { context, contentType, subPath };
+
+		Task result = (Task)method.Invoke(_middleware, parameters)!;
+
+		return result;
+	}
+
+	public void PathMismatch(string path) {
+		_logger.LogDebug("The request path {Path} does not match the path filter", path);
+	}
+
+	public void FileTypeNotSupported(string path) {
+		_logger.LogDebug("The request path {Path} does not match a supported file type", path);
+	}
+
+	/// <summary>
+	/// Processes a request to determine if it matches a known file, and if so, serves it.
+	/// This is a reflected implementation of AspNetCore StaticFileMiddleware.
+	/// </summary>
+	/// <param name="context"></param>
+	/// <returns></returns>
 	public Task Invoke(HttpContext context) {
-		if (context.Request.Path.StartsWithSegments("/apps")) {
+		if (!context.Request.Path.StartsWithSegments("/apps"))
+			return _middleware.Invoke(context);
+
+		// Add something that checks searchparams for a fallback=false.
+		// If fallback is false, do not perform the path mutation stuff.
+
+		if (!context.Request.Path.ToString().EndsWith(".js")) {
 			var mutatedPath = context.Request.Path.ToString().Trim('/').Split('/');
 			var newPath = '/' + string.Join('/',mutatedPath.Take(3).ToArray()) + "/index.html";
-
 			context.Request.Path = newPath;
 
-			return staticFileMiddleware.Value.Invoke(context);
+			//context.Response.Headers.CacheControl =
+			//	"public, max-age=31536000, s-maxage=31536000, immutable";
 		}
 
-		return _next(context);
-	}
+		if (!ValidatePath(context, _matchUrl, out PathString subPath)) {
+			PathMismatch(subPath);
+		}
+		else if (!LookupContentType(_contentTypeProvider, _options, subPath, out var contentType)) {
+			FileTypeNotSupported(subPath);
+		}
+		else {
+			// If we get here, we can try to serve the file
+			return TryServeStaticFile(context, contentType, subPath);
+		}
 
+		return _middleware.Invoke(context);
+	}
 }
